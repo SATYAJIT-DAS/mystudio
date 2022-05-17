a72bdb7ae699fb0506e1194e3e4e5632f048c808
SATYAJIT-DAS <sjcdas@gmail.com>
2022-05-17 08:43:32 -0400

    first commit

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.playtubescript.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | PlayTube - The Ultimate Video Sharing Platform
+// | Copyright (c) 2017 PlayTube. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "mystudio_root";
+// MySQL Database Password
+$sql_db_pass = "RYg2oM%~U&JN";
+// MySQL Database Name
+$sql_db_name = "mystudio_db";
+
+// Site URL
+$site_url = "https://www.mystudio07.com"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "123456789"; // Your purchase code, don't give it to anyone. 
+?>
